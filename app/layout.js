431c8c638c47b01c2e8251d1b6bfc89ed4257803
mahajan yash sahebrao <mahajanyashsahebrao@mahajans-MacBook-Air.local>
2026-07-09 21:49:11 +0530

    Make Instagram icon itself clickable to Satyanand's profile

--- app/layout.js
+++ app/layout.js
@@ -116,7 +116,7 @@ export default function RootLayout({ children }) {
               </a>
 
               <div className="relative group flex items-center justify-center cursor-pointer">
-                <div className="hover:text-white hover:drop-shadow-[0_0_10px_currentColor] hover:scale-110 transition-all duration-300 transform" style={{ color: "#E1306C" }}>
+                <a href="https://www.instagram.com/satyagupta.dev/" target="_blank" rel="noopener noreferrer" className="hover:text-white hover:drop-shadow-[0_0_10px_currentColor] hover:scale-110 transition-all duration-300 transform" style={{ color: "#E1306C", textDecoration: "none" }}>
                   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                     <defs>
                       <linearGradient id="ig-grad" x1="10%" y1="100%" x2="90%" y2="0%">
@@ -129,7 +129,7 @@ export default function RootLayout({ children }) {
                     </defs>
                     <path fill="url(#ig-grad)" d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 1 0 0 12.324 6.162 6.162 0 0 0 0-12.324zM12 16a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm6.406-11.845a1.44 1.44 0 1 0 0 2.881 1.44 1.44 0 0 0 0-2.881z"/>
                   </svg>
-                </div>
+                </a>
                 <div className="absolute bottom-full hidden group-hover:flex flex-col pb-2 z-50 transform -translate-x-1/2 left-1/2">
                   <div className="flex flex-col bg-[rgba(11,11,13,0.9)] border border-[var(--vault-outline)] rounded-lg shadow-xl overflow-hidden min-w-[140px]">
                     <a href="https://www.instagram.com/satyagupta.dev/" target="_blank" rel="noopener noreferrer" className="px-4 py-3 hover:bg-[var(--heist-red)] hover:text-white transition-colors text-xs text-center whitespace-nowrap">
